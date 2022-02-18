c320157d1795764493abcb0198d219a0b63df95b
Jen Martin <jmart427@ucsc.edu>
2022-02-18 13:23:02 -0800

    adds a CA-statewide analysis to data processing

--- data/process_metadata.py
+++ data/process_metadata.py
@@ -21,15 +21,21 @@ def process_metadata(conversion, metadata):
     county_state = {"Washington","Delaware","Wyoming","Ohio","Nevada","Mississippi","Texas","Iowa","Oklahoma","Utah","Indiana","Colorado","Arkansas","Idaho","Oregon","Hawaii","New York"}
 
     mfiles = metadata.split(",") #get names of metadata files
-    metadata = open("metadata_merged.tsv","w+") #output file for merged metadata
+    metadata = open("metadata_merged.tsv","w+") #output file for merged metadata, for US + CA county analysis
+    metadata_us = open("metadata_merged_us.tsv","w+") #output file for merged metadata, for US analysis
     badsamples = open("unlabeled_samples.txt","w+") # file for rejected sample names
-    region_assoc = open("sample_regions.tsv","w+") # file to store associations between sample ID and region name
-    date_file = open("sample_dates.tsv","w+") # file to store associations between sample ID and sample dates
+    region_assoc = open("sample_regions.tsv","w+") # file to store associations between sample ID and region name, for US + CA county analysis
+    region_assoc_us = open("sample_regions_us.tsv","w+") # file to store associations between sample ID and region name, for US analysis
+    date_file = open("sample_dates.tsv","w+") # file to store associations between sample ID and sample dates, for US + CA county analysis
+    date_file_us = open("sample_dates_us.tsv","w+") # file to store associations between sample ID and sample dates, for US analysis
     print("sample_id\tdate", file = date_file)
+    print("sample_id\tdate", file = date_file_us)
     pid_assoc = open("pids.tsv","w+") # file to store associations between sample ID and personal IDs (paui's)
+    pid_assoc_us = open("pids_us.tsv","w+") # file to store associations between sample ID and personal IDs (paui's)
     date_pattern = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
     #write metadata header
     print("strain\tname\tpangolin_lineage\tnextclade_clade\tgisaid_accession\tcounty\tdate\tlink_id\tsequencing_lab\tgenbank_accession\tcountry", file = metadata)
+    print("strain\tname\tpangolin_lineage\tnextclade_clade\tgisaid_accession\tcounty\tdate\tlink_id\tsequencing_lab\tgenbank_accession\tcountry", file = metadata_us)
     for f in mfiles:
         with open(f) as inf:
             fields = inf.readline().strip().split("\t")
@@ -40,16 +46,26 @@ def process_metadata(conversion, metadata):
                     fields = entry.split("\t")
                     for i in range(len(fields)):
                         fields[i] = fields[i].strip()
-                    # check for valid California county names
                     county = fields[5]
-                    if county != "":
-                        #check if county is in lexicon
-                        if county.upper() in (c.upper() for c in conversion): # convert all names to uppercase to avoid differences in case
-                            #check for valid date
-                            if re.search(date_pattern, fields[6]):
+                    #check for valid date
+                    if re.search(date_pattern, fields[6]):
+                        fields.append("")
+                        fields.append("USA")
+                        #add item to merged metadata file for CA state analysis
+                        print("\t".join(fields), file = metadata_us)
+                        #assign to CA state in region file
+                        print(fields[0] + "\tCalifornia", file = region_assoc_us)
+                        #if non-standard sample name, add sample ID and date to sample dates file
+                        if not (fields[0].startswith("USA/")):
+                            print(fields[0] + "\t" + fields[6], file = date_file_us)
+                        #add PAUI to association file
+                        if fields[7] != "":
+                            print(fields[0] + "\t" + fields[7], file = pid_assoc_us)
+                        # check for valid California county names for County specific data processing
+                        if county != "":
+                            #check if county is in lexicon
+                            if county.upper() in (c.upper() for c in conversion): # convert all names to uppercase to avoid differences in case
                                 #add item to merged metadata file
-                                fields.append("")
-                                fields.append("USA")
                                 print("\t".join(fields), file = metadata)
                                 #add sample ID and county name to sample regions file
                                 if county in county_state: #handle cases where county name matches state name
@@ -63,12 +79,8 @@ def process_metadata(conversion, metadata):
                                 #add PAUI to association file
                                 if fields[7] != "":
                                     print(fields[0] + "\t" + fields[7], file = pid_assoc)
-                            else:
-                                print(fields[0], file = badsamples) #does not have a valid date
-                        else:
-                            print(fields[0], file = badsamples) #not a valid CA county
                     else:
-                        print(fields[0], file = badsamples) #sample does not have county
+                        print(fields[0], file = badsamples) #does not have a valid date        
             else:
                 # public metadata header: strain,genbank_accession,date,country,host,completeness,length,Nextstrain_clade,pangolin_lineage,Nextstrain_clade_usher,pango_lineage_usher
                 for entry in inf:
@@ -78,47 +90,47 @@ def process_metadata(conversion, metadata):
                     if country == "USA":
                         state = fields[0].split("/")[1].split("-")[0]
                         if state in conversion:
-                            #TO DO - assign California samples a county via naming scheme
-                            if state == "CA":
-                                #sample is from California; reject
-                                print(fields[0], file = badsamples)
-                            else: 
-                                #check for valid date
-                                if re.search(date_pattern, fields[2]):
-                                    #add item to merged metadata file
-                                    newfields = []
-                                    newfields.append(fields[0]) #sample name
-                                    newfields.append("") #CDPH name
-                                    newfields.append(fields[8]) #pangolin_lineage
-                                    newfields.append(fields[7]) #nextclade_clade = Nextstrain_clade
-                                    newfields.append("") #gisaid_accession
-                                    newfields.append("") #county name
-                                    newfields.append(fields[2]) #date
-                                    newfields.append("") #Link ID (PAUI)
-                                    newfields.append("") #sequencing_lab
-                                    newfields.append(fields[1]) #genbank_accession
-                                    newfields.append("USA") #country
+                            #check for valid date
+                            if re.search(date_pattern, fields[2]):
+                                #add item to merged metadata file
+                                newfields = []
+                                newfields.append(fields[0]) #sample name
+                                newfields.append("") #CDPH name
+                                newfields.append(fields[8]) #pangolin_lineage
+                                newfields.append(fields[7]) #nextclade_clade = Nextstrain_clade
+                                newfields.append("") #gisaid_accession
+                                newfields.append("") #county name
+                                newfields.append(fields[2]) #date
+                                newfields.append("") #Link ID (PAUI)
+                                newfields.append("") #sequencing_lab
+                                newfields.append(fields[1]) #genbank_accession
+                                newfields.append("USA") #country
+                                print("\t".join(newfields), file = metadata_us)
+                                #add sample ID and state name to sample regions file
+                                text = conversion[state.upper()].replace(" ", "_")
+                                print(fields[0] + "\t" + text, file = region_assoc_us)
+                                #TO DO - assign California samples a county via naming scheme
+                                #filter out CA samples for CA county analysis
+                                if state != "CA":
                                     print("\t".join(newfields), file = metadata)
-                                    #add sample ID and state name to sample regions file
-                                    text = conversion[state.upper()].replace(" ", "_")
                                     print(fields[0] + "\t" + text, file = region_assoc)
-                                else:
-                                    print(fields[0], file = badsamples) #does not have a valid date
+                            else:
+                                print(fields[0], file = badsamples) #does not have a valid date   
                         else:
                             #can't parse state
                             print(fields[0], file = badsamples)
-                    elif entry.startswith("CDPH"): 
-                        #sample is from California; reject
-                        print(fields[0], file = badsamples)
                     else:
                         #non-US sample
                         print(fields[0], file = badsamples)
-
     metadata.close()
     badsamples.close()
     region_assoc.close()
     date_file.close()
     pid_assoc.close()
+    metadata_us.close()
+    region_assoc_us.close()
+    date_file_us.close()
+    pid_assoc_us.close()
 
 lexicon = {"Alameda":"Alameda County","Alpine":"Alpine County","Amador":"Amador County","Butte":"Butte County",
     "Calaveras":"Calaveras County","Colusa":"Colusa County","Contra Costa":"Contra Costa County","Del Norte":"Del Norte County",
@@ -150,7 +162,7 @@ lexicon = {"Alameda":"Alameda County","Alpine":"Alpine County","Amador":"Amador 
     "SOLANO":"Solano County","SONOMA":"Sonoma County","STANISLAUS":"Stanislaus County","SUTTER":"Sutter County",
     "TEHAMA":"Tehama County","TRINITY":"Trinity County","TULARE":"Tulare County","TUOLUMNE":"Tuolumne County",
     "VENTURA":"Ventura County","YOLO":"Yolo County","YUBA":"Yuba County",
-    "AL":"Alabama","AK":"Alaska","AR":"Arkansas","AZ":"Arizona","CO":"Colorado",
+    "AL":"Alabama","AK":"Alaska","AR":"Arkansas","AZ":"Arizona","CA":"California","CO":"Colorado",
     "CT":"Connecticut","DE":"Delaware","DC":"District of Columbia","FL":"Florida","GA":"Georgia","HI":"Hawaii",
     "ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine",
     "MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana",
--- data/update_js.py
+++ data/update_js.py
@@ -5,116 +5,126 @@ import json
 import math
 import datetime as dt
 from dateutil.relativedelta import relativedelta
-#cdf = pd.read_csv('hardcoded_clusters.tsv',sep='\t')
-def update_js(target, conversion = {}):
-    svd = {"type":"FeatureCollection", "features":[]}
+def update_js(target, conversion = {}, extension =""):
+    geo_files = target.split(",") #get name(s) of geojson files
+    ext = extension.split(",") #get file name extensions if processiing more than one file
     monthswap = {"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
     conversion.update({v:v for k,v in conversion.items()})
     conversion["indeterminate"] = "indeterminate"
-    #print(conversion)
-    #ivc = cdf.region.value_counts()
     datepoints = ["all", dt.date.today()-relativedelta(months=12), dt.date.today()-relativedelta(months=6), dt.date.today()-relativedelta(months=3)]
-    #here, the data is stored in a series of dictionaries
-    #which are generally structured with the minimum date as the outermost layer
-    #then the destination of an introduction
-    #than the source of an introduction
-    prefd = {datepoints[0]:"", datepoints[1]:"12_", datepoints[2]:"6_", datepoints[3]:"3_"}
-    dinvc = {d:{} for d in datepoints}
-    dsvc = {d:{} for d in datepoints}
-    dotvc = {d:{} for d in datepoints}
-    dovc = {d:{} for d in datepoints}
-    with open("hardcoded_clusters.tsv") as inf:
-        for entry in inf:
-            spent = entry.strip().split("\t")
-            if spent[0] == "cluster_id":
-                continue
-            reg = conversion[spent[9].replace("_"," ")]
-            if spent[10] == "indeterminate":
-                continue
-            #get the date of this cluster's earliest sample into a usable form
-            dsplt = spent[2].split("-")
-            if dsplt == "no-valid-date".split("-"):
-                cdate = dt.date(year=2019,month=11,day=1)
-            else:
-                cdate = dt.date(year=int(dsplt[0]), month=int(monthswap[dsplt[1]]), day=int(dsplt[2]))
-            for startdate, ovc in dovc.items():
-                if startdate == "all" or cdate > startdate:
-                    if reg not in dsvc[startdate]:
-                        dsvc[startdate][reg] = 0
-                    dsvc[startdate][reg] += spent[-1].count(',') + 1
-                    if reg not in dinvc[startdate]:
-                        dinvc[startdate][reg] = 0
-                    dinvc[startdate][reg] += 1
-                    otvc = dotvc[startdate]
-                    ovc = dovc[startdate]
-                    if reg not in ovc:
-                        ovc[reg] = {}
-                    for tlo in spent[10].split(","):
-                        orig = conversion[tlo.replace("_"," ")]
-                        if orig not in otvc:
-                            otvc[orig] = 0
-                        otvc[orig] += 1
-                        if orig not in ovc[reg]:
-                            ovc[reg][orig] = 0
-                        ovc[reg][orig] += 1
-    dsumin = {sd:sum(invc.values()) for sd,invc in dinvc.items()}
-    sids = {}
-    f = open(target)
-    geojson_lines = json.load(f)
-    f.close()
-    id = 0
-    #we fill in the basic count of introductions to each area first
-    #as well as fill in an integer "ID" if its not already present
-    for data in geojson_lines["features"]:
-        data["properties"]["intros"] = {}
-        for sd, invc in dinvc.items():
-            prefix = prefd[sd]
-            data["properties"]["intros"][prefix + "basecount"] = invc.get(data["properties"]["name"],0) 
-        svd["features"].append(data)
-        if "id" in data:
-            sids[data["properties"]["name"]] = data["id"]
-        else:
-            data["id"] = str(id)
-            sids[data["properties"]["name"]] = str(id)
-            id += 1
-    #update the data intros list with specific state values
-    for ftd in svd["features"]:
-        #update the ftd["properties"]["intros"] with each state
-        #state introductions to itself, for now, I will fill with indeterminate
-        #this is transposed so that the introductions to each state are stored across each other state by origin
-        #in order that coloring and hovertext can be correctly accessed.
-        iid = ftd['properties']["name"]
-        #for timeslice
-        for sd, ovc in dovc.items():
-            #get everything where this specific row/region is an origin
-            prefix = prefd[sd]
-            #fill with 0
-            inv_ovc = {k:subd.get(iid,0) for k,subd in ovc.items()}
-            #print(inv_ovc)
-            for destination, count in inv_ovc.items():
-                #scale the count for display
-                if destination == "indeterminate":
+    
+    for idx, gfile in enumerate(geo_files):
+        #intialize variables
+        svd = {"type":"FeatureCollection", "features":[]}
+        #here, the data is stored in a series of dictionaries
+        #which are generally structured with the minimum date as the outermost layer
+        #then the destination of an introduction
+        #than the source of an introduction
+        prefd = {datepoints[0]:"", datepoints[1]:"12_", datepoints[2]:"6_", datepoints[3]:"3_"}
+        dinvc = {d:{} for d in datepoints}
+        dsvc = {d:{} for d in datepoints}
+        dotvc = {d:{} for d in datepoints}
+        dovc = {d:{} for d in datepoints}
+
+        #set filenames
+        if idx > (len(ext) - 1):
+            ext[idx] = ""
+        cluster_file = "hardcoded_clusters" + ext[idx] + ".tsv"
+        region_file = "regions" + ext[idx] + ".js" 
+        with open(cluster_file) as inf:
+            for entry in inf:
+                spent = entry.strip().split("\t")
+                if spent[0] == "cluster_id":
                     continue
-                did = sids[conversion.get(destination,destination)]
-                ftd["properties"]["intros"][prefix + "raw" + did] = count
-                if count > 0: #debug: if count > 5:
-                    sumin = dsumin[sd]
-                    invc = dinvc[sd]
-                    otvc = dotvc[sd]
-                    ftd["properties"]["intros"][prefix + did] = math.log10(count * sumin / invc[destination] / otvc[iid])
+                reg = conversion[spent[9].replace("_"," ")]
+                if spent[10] == "indeterminate":
+                    continue
+                #get the date of this cluster's earliest sample into a usable form
+                dsplt = spent[2].split("-")
+                if dsplt == "no-valid-date".split("-"):
+                    cdate = dt.date(year=2019,month=11,day=1)
                 else:
-                    #if there are less than 5 counts, the log correction can do some pretty extreme highlighting
-                    #for example, even a single introduction between two distant places may be surprising
-                    #but that doesn't mean it should get a lot of emphasis. So we cut off anything with less than 5 introductions total.
-                    ftd["properties"]["intros"][prefix + did] = -0.5
-    with open("regions.js","w") as outf:
-        print("//data updated via update_js.py",file=outf)
-        print('var None = "None"',file=outf)
-        print('var introData = {"type":"FeatureCollection","features":[',file=outf)
-        for propd in svd['features']:
-            assert "intros" in propd["properties"]
-            print(str(propd) + ",",file=outf)
-        print("]};",file=outf)
+                    cdate = dt.date(year=int(dsplt[0]), month=int(monthswap[dsplt[1]]), day=int(dsplt[2]))
+                for startdate, ovc in dovc.items():
+                    if startdate == "all" or cdate > startdate:
+                        if reg not in dsvc[startdate]:
+                            dsvc[startdate][reg] = 0
+                        dsvc[startdate][reg] += spent[-1].count(',') + 1
+                        if reg not in dinvc[startdate]:
+                            dinvc[startdate][reg] = 0
+                        dinvc[startdate][reg] += 1
+                        otvc = dotvc[startdate]
+                        ovc = dovc[startdate]
+                        if reg not in ovc:
+                            ovc[reg] = {}
+                        for tlo in spent[10].split(","):
+                            orig = conversion[tlo.replace("_"," ")]
+                            if orig not in otvc:
+                                otvc[orig] = 0
+                            otvc[orig] += 1
+                            if orig not in ovc[reg]:
+                                ovc[reg][orig] = 0
+                            ovc[reg][orig] += 1
+        dsumin = {sd:sum(invc.values()) for sd,invc in dinvc.items()}
+        sids = {}
+        f = open(gfile)
+        geojson_lines = json.load(f)
+        f.close()
+        id = 0
+        #we fill in the basic count of introductions to each area first
+        #as well as fill in an integer "ID" if its not already present
+        for data in geojson_lines["features"]:
+            data["properties"]["intros"] = {}
+            for sd, invc in dinvc.items():
+                prefix = prefd[sd]
+                data["properties"]["intros"][prefix + "basecount"] = invc.get(data["properties"]["name"],0) 
+            svd["features"].append(data)
+            if "id" in data:
+                sids[data["properties"]["name"]] = data["id"]
+            else:
+                data["id"] = str(id)
+                sids[data["properties"]["name"]] = str(id)
+                id += 1
+        #update the data intros list with specific state values
+        for ftd in svd["features"]:
+            #update the ftd["properties"]["intros"] with each state
+            #state introductions to itself, for now, I will fill with indeterminate
+            #this is transposed so that the introductions to each state are stored across each other state by origin
+            #in order that coloring and hovertext can be correctly accessed.
+            iid = ftd['properties']["name"]
+            did = {}
+            #for timeslice
+            for sd, ovc in dovc.items():
+                #get everything where this specific row/region is an origin
+                prefix = prefd[sd]
+                #fill with 0
+                inv_ovc = {k:subd.get(iid,0) for k,subd in ovc.items()}
+                for destination, count in inv_ovc.items():
+                    #scale the count for display
+                    if destination == "indeterminate":
+                        continue
+                    did = sids[conversion.get(destination,destination)]
+                    ftd["properties"]["intros"][prefix + "raw" + did] = count
+                    if count > 0: #debug: if count > 5:
+                        sumin = dsumin[sd]
+                        invc = dinvc[sd]
+                        otvc = dotvc[sd]
+                        ftd["properties"]["intros"][prefix + did] = math.log10(count * sumin / invc[destination] / otvc[iid])
+                    else:
+                        #if there are less than 5 counts, the log correction can do some pretty extreme highlighting
+                        #for example, even a single introduction between two distant places may be surprising
+                        #but that doesn't mean it should get a lot of emphasis. So we cut off anything with less than 5 introductions total.
+                        ftd["properties"]["intros"][prefix + did] = -0.5
+        with open(region_file,"w") as outf:
+            print("//data updated via update_js.py",file=outf)
+            if idx == 0:
+                print('var None = "None";',file=outf)
+            print('var introData' + ext[idx] + ' = {"type":"FeatureCollection","features":[',file=outf)
+            for propd in svd['features']:
+                assert "intros" in propd["properties"]
+                print(str(propd) + ",",file=outf)
+            print("]};",file=outf)
+
 lexicon = {"Alameda":"Alameda County","Alpine":"Alpine County","Amador":"Amador County","Butte":"Butte County",
     "Calaveras":"Calaveras County","Colusa":"Colusa County","Contra Costa":"Contra Costa County","Del Norte":"Del Norte County",
     "El Dorado":"El Dorado County","Fresno":"Fresno County","Glenn":"Glenn County","Humboldt":"Humboldt County",
@@ -145,7 +155,7 @@ lexicon = {"Alameda":"Alameda County","Alpine":"Alpine County","Amador":"Amador 
     "SOLANO":"Solano County","SONOMA":"Sonoma County","STANISLAUS":"Stanislaus County","SUTTER":"Sutter County",
     "TEHAMA":"Tehama County","TRINITY":"Trinity County","TULARE":"Tulare County","TUOLUMNE":"Tuolumne County",
     "VENTURA":"Ventura County","YOLO":"Yolo County","YUBA":"Yuba County",
-    "AL":"Alabama","AK":"Alaska","AR":"Arkansas","AZ":"Arizona","CO":"Colorado",
+    "AL":"Alabama","AK":"Alaska","AR":"Arkansas","AZ":"Arizona","CA":"California","CO":"Colorado",
     "CT":"Connecticut","DE":"Delaware","DC":"District of Columbia","FL":"Florida","GA":"Georgia","HI":"Hawaii",
     "ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine",
     "MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana",
@@ -153,5 +163,6 @@ lexicon = {"Alameda":"Alameda County","Alpine":"Alpine County","Amador":"Amador 
     "ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island",
     "SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia",
     "WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming","PR":"Puerto Rico"}
+extension = ",_us"
 if __name__ == "__main__":
-    update_js("us-states_ca-counties.geo.json",lexicon)
+    update_js("us-states_ca-counties.geo.json,us-states.geo.json",lexicon, extension)
