3084a6ad4100373fbb37271aba312357844f7a11
Jen Martin <jmart427@ucsc.edu>
2022-08-02 19:11:46 -0700

    generates 2 json files instead of multiple tsv files for the data table

--- src/python/generate_display_tables.py
+++ src/python/generate_display_tables.py
@@ -1,232 +1,161 @@
-#  Python "backend" code to generate individual cluster files for each 
-#    region. These files contain the information that is displayed in
+#  Python "backend" code to generate JSON files to display cluster data in
 #    the table below the map on the web page. 
 #  This script is meant to be called from "master_backend.py" but can be
 #    run separately from the command line.
 #
-# "generate_display_tables" is the main function.
 # Arguments:
-#   -conversion: dictionary created from lexicon file
-#   -host: Publicly accessible URL where you will be hosting the Taxonium
-#     protobuf.
 #   -extension: if using more than one geojson file this is list of file
 #     name extensions to differentiate each set of files. Specify only the
 #     file name extensions to use with the 2nd, 3rd, ..., set of files.
+#   -isWDL: defalut is False. Set to true only if the script will be run 
+#     as a WDL task in Terra.
 # Outputs:
-#  - default_clusters.tsv
-#  -[region_name]_topclusters.tsv
-#  -cluster_labels.tsv
+#  - "cluster-data.json.gz" #gzipped JSON file with basic cluster information (no sample names)
+#  - "sample-data.json.gz" #gzipped JSON file with sample names for each cluster
+#  - "cluster_pids.json" #for CDPH Investigator tool, links cluster ID and PAUI's/specimen IDs
 #
 # Example command line usage:
-# python3 generate_display_tables.py -l state_and_county_lexicon.txt
-#  -H https://storage.googleapis.com/ucsc-gi-cdph-bigtree/display_tables/ -e "_us"
+# python3 generate_display_tables.py -e "_us"
 #-------------------------------------------------------------
 # CDPH WDL INPUTS:
-#  File state_and_county_lex # "state_and_county_lexicon.txt", lexicon with state and county abbreviations
 #  File clusters_counties # "hardcoded_clusters.tsv", cluster file from CA county introductions
 #  File clusters_state # "hardcoded_clusters_us.tsv", cluster file from CA state introductions
-#  File pids #"pids.tsv", file linking sample name and PAUI/Specimen ID from CA county samples
-#  File pids_us #"pids_us.tsv", file linking sample name and PAUI/Specimen ID from CA state samples
+#  File pids #"pids.tsv", file linking sample name and PAUI/Specimen ID
 # CDPH WDL OUTPUTS:
-#  File labels = "cluster_labels.tsv"
-#  File labels_us = "cluster_labels_us.tsv"
-#  Array[File] display_tables = glob("*lusters.tsv")
+#  "cluster-data.json.gz" and "cluster_data_us.json.gz" #gzipped JSON file with basic cluster information (no sample names)
+#  "sample-data.json.gz" and "sample_data_us.json.gz" #gzipped JSON file with sample names for each cluster
+#  "cluster_pids.json" and "cluster_pids_us.json" #for CDPH Investigator tool, links cluster ID and PAUI's/specimen IDs
 #-------------------------------------------------------------
 
+import json, gzip
 import os #comment out for WDL
 
-# function to construct links to Taxonium
-def get_taxonium_link(host,cluster,ext):
-    link = 'https://taxonium.org/?protoUrl=' + host + 'cview' + ext + '.jsonl.gz'
-    link += '&color={"field":"meta_region"}'
-    link += '&srch=[{"key":"aa1","type":"meta_cluster","method":"text_match","text":"'
-    link += cluster
-    link += '","gene":"S","position":484,"new_residue":"any","min_tips":0,"controls":true}]'
-    link += '&zoomToSearch=0'
-    return link
+def generate_display_tables(extension = [''], isWDL = False):
+    #== for WDL ===
+    # isWDL = True
+    #===
+    
 
-
-#function to evaluate whether to add custom CDPH data fields to 
-# output cluster files
-def is_custom_data(host):
-    if "ucsc-gi-cdph-bigtree" in host:
-        return True
-    else:
-        return False
-
-## Functions for custom CDPH data
-#adds a link to the CA Big Tree Investigator to the output cluster files
-def get_investigator_link(pid_list,cluster_id,fname):
-    link = "No identifiable samples"
-    if pid_list != "":
-        link = "https://investigator.big-tree.ucsc.edu/?"
-        link += "file=" + fname
-        link += "&cid=" + cluster_id
-        #encode spaces
-        link = link.replace(" ","%20")
-    return link
-
-#adds unique sample ID's to the output cluster files
-def get_sample_pauis(items,pid_assoc):
-    pid_list = ""
-    pids = []
-    samples = items.split(",")
-    for s in samples:
-        #get PAUI's from sample names
-        if s.startswith('CDPH') or s.startswith('USA/CA-CDPH'):
+    #For CDPH data, returns an array containing the unique sample ID's for
+    #each sample in a cluster
+    def get_sample_pauis(items,pid_assoc):
+        pids_arr = []
+        samples = items.split(",")
+        for s in samples:
+            #get PAUI's from sample names
             if s in pid_assoc:
-                pids.append(pid_assoc[s])
-    if pids:
-        pid_list = ",".join(pids)
-    return pid_list
+                pids_arr.append(pid_assoc[s])
+        return pids_arr
 
-## main function
-def generate_display_tables(conversion = {}, host = '', extension = ['']):
-    #for WDL: insert here read_lexicon function from utils.py
-    
-    #conversion = read_lexicon('~{state_and_county_lex}') #for WDL
-    #host = 'https://storage.googleapis.com/ucsc-gi-cdph-bigtree/display_tables/'
-    #extension = ['', '_us'] #for WDL
-
+    #function to evaluate whether to add custom CDPH data fields to output cluster files
+    def is_custom_data():
+        if len(extension) > 1:
+            return True
+        else:
+            return False
     #set flag to identify whether to add custom fields to output cluster files
-    is_custom =  is_custom_data(host)
+    is_custom =  is_custom_data()
 
-    #input file names
-    cluster_file = ["hardcoded_clusters" + extension[i] + ".tsv" for i in range(len(extension))] #comment out for WDL
-    #cluster_file = ['~{clusters_counties}', '~{clusters_state}'] #for WDL
-    if is_custom:
-        pid_file = ["pids" + extension[i] + ".tsv" for i in range(len(extension))] #comment out for WDL
-        #pid_file = ['~{pids}','~{pids_us}'] #for WDL
-        subfolder = ""
+
+    if isWDL:
+        extension = ['', '_us']
+        #input file names
+        cluster_file = ['~{clusters_counties}', '~{clusters_state}']
+        pid_file = '~{pids}'
     else:
-        subfolder = "display_tables/"
-        #check if folder exists and create if needed
-        if not os.path.isdir(subfolder):
-            os.makedirs(subfolder)
+        #input file names
+        cluster_file = ["hardcoded_clusters" + extension[i] + ".tsv" for i in range(len(extension))]
+        if is_custom:
+            pid_file = "pids.tsv"
+
+    # function to add quotes around a variable for JSON formatting
+    def addq(item):
+        return "\"" + item + "\""
+
+    def fix_month(datestr):
+        monthswap = {"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
+        splitr = datestr.split("-")
+        return splitr[0] + "-" + monthswap.get(splitr[1],splitr[1]) + "-" + splitr[2]
     
-    #file header
-    header = "Cluster ID\tRegion\tSample Count\tEarliest Date\tLatest Date\tClade\tLineage\tInferred Origins\tInferred Origin Confidences\tGrowth Score\tClick to View in Taxonium"
+    # get sample name/PAUI associations
     if is_custom:
-        header += "\tClick to View in CA Big Tree Investigator\tSpecimen IDs"
+        pid_assoc = {} # links sample ID with personal id (PAUI)
+        with open(pid_file) as inf:
+            for entry in inf:
+                spent = entry.strip().split("\t")
+                pid_assoc[spent[0]] = spent[1]
 
     for i, ext in enumerate(extension):
-        labels_file = "cluster_labels" + ext + ".tsv"
-        defaultc_file = "default_clusters" + ext + ".tsv"
-        filelines = {}
-        def fix_month(datestr):
-            monthswap = {"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
-            splitr = datestr.split("-")
-            return splitr[0] + "-" + monthswap.get(splitr[1],splitr[1]) + "-" + splitr[2]
-        default_growthvs = []
-        default_lines = []
-        totbuff = [] #track overall no valid date clusters to fill in at the end.
-        
         if is_custom:
-            # get sample name/PAUI associations
-            pid_assoc = {} # links sample ID with personal id (PAUI)
-            with open(pid_file[i]) as inf:
-                for entry in inf:
-                    spent = entry.strip().split("\t")
-                    pid_assoc[spent[0]] = spent[1]
-
+           sample_pids = {} # stores association between cluster ID and PAUIs
+        
+        # get clusters data and put into array
+        cluster_data = []
         with open(cluster_file[i]) as inf:
-            cr = "None"
-            buffer = []
             for entry in inf:
                 spent = entry.strip().split("\t")
                 if spent[0] == "cluster_id": 
                     continue
-                reg = conversion[spent[9].replace("_"," ")]
-                if reg not in filelines:
-                    filelines[reg] = []
-                if cr == "None":
-                    cr = reg
-                elif reg != cr:
-                    #when moving to a new region
-                    if len(filelines[cr]) < 100:
-                        filelines[cr].extend(buffer[:100-len(filelines[cr])])
-                    buffer = []
-                    cr = reg
-                if spent[3] == "no-valid-date":
-                    buffer.append(entry.strip())
-                    totbuff.append((entry.strip(), float(spent[4])))
-                    continue
-                if len(filelines[reg]) < 100:
-                    filelines[reg].append(entry.strip())
-                #now, check to see if this scores in the top 100 overall. Significantly more complicated since we have to sort things out as we go here.
-                if len(default_lines) < 100:
-                    default_growthvs.append(float(spent[4]))
-                    default_lines.append(entry.strip())
-                elif float(spent[4]) > min(default_growthvs):
-                    popind = default_growthvs.index(min(default_growthvs))
-                    default_growthvs.pop(popind)
-                    default_lines.pop(popind)
-                    default_growthvs.append(float(spent[4]))
-                    default_lines.append(entry.strip())
-                    assert len(default_lines) == 100
-            #remove any remaining buffer for the last region in the file as well.
-            if len(filelines[cr]) < 100:
-                filelines[cr].extend(buffer[:100-len(filelines[cr])])
-            #and if there are less than 100 clusters with dates for the default view, extend that as well.
-            if len(default_lines) < 100:
-                totbuff.sort(key = lambda x: x[1], reverse = True)
-                for t in totbuff[:100-len(default_lines)]:
-                    default_lines.append(t[0])
-                    default_growthvs.append(0-1/t[1])
-        mout = open(labels_file,"w+")
-        print("sample\tcluster",file=mout)
-        for reg, lines in filelines.items():
-            fname = conversion[reg].replace(" ", "_") + "_topclusters" + ext + ".tsv"
-            with open(subfolder + fname, "w+") as outf:
-                print(header,file=outf)
-                for l in lines:
-                    #process the line 
-                    #into something more parseable.
-                    spent = l.split("\t")
-                    #save matching results to the other output files
-                    #for downstream extraction of json
-                    samples = spent[-1].split(",")
-                    for s in samples:
-                        print(s + "\t" + spent[0],file=mout)
-                    #generate a link to exist in the last column
-                    #based on the global "host" variable.
-                    #and including all html syntax.
-                    link = get_taxonium_link(host,spent[0],ext)
-                    #additionally process the date strings
-                    outline = [spent[0], spent[9], spent[1], fix_month(spent[2]), fix_month(spent[3]), spent[12], spent[13], spent[10], spent[11], spent[4], link]
-                    if is_custom:
-                        #get California sample PAUIs and create link to BT Investigator
-                        pid_list = get_sample_pauis(spent[-1],pid_assoc)
-                        ilink = get_investigator_link(pid_list, spent[0], fname)
-                        outline.append(ilink)
-                        outline.append(pid_list)
-                    print("\t".join(outline),file=outf)
-
-        mout.close()
-        sorted_defaults = sorted(list(zip(default_growthvs,default_lines)),key=lambda x:-x[0])
-        with open(subfolder + defaultc_file,"w+") as outf:
-            print(header,file=outf)
-            for gv,dl in sorted_defaults:
-                spent = dl.split("\t")
-                link = get_taxonium_link(host,spent[0],ext)
-                outline = [spent[0], spent[9], spent[1], fix_month(spent[2]), fix_month(spent[3]), spent[12], spent[13], spent[10], spent[11], spent[4], link]
+                #fix date format
+                spent[2] = fix_month(spent[2])
+                spent[3] = fix_month(spent[3])
                 if is_custom:
-                    #get California sample PAUIs and create link to BT Investigator
-                    pid_list = get_sample_pauis(spent[-1],pid_assoc)
-                    ilink = get_investigator_link(pid_list, spent[0],defaultc_file)
-                    outline.append(ilink)
-                    outline.append(pid_list)
-                print("\t".join(outline), file = outf)
+                    #add California sample PAUIs
+                    ids = ""
+                    pids_arr = get_sample_pauis(spent[-1],pid_assoc)
+                    if pids_arr:
+                        sample_pids[spent[0]] = pids_arr
+                        ids = ",".join(pids_arr)
+                    spent.append(ids)
+                cluster_data.append(spent)
+        
+        #now, sort by growth score
+        cluster_data.sort(key = lambda x: x[4], reverse = True)
+
+        #output data to be compatible with parse.JSON
+        # -create as compact a string as possible,
+        # -only add quotes to items that are strings to save space
+        txt_data = "["
+        txt_samples = "["
+        for i, d in enumerate(cluster_data):
+            outline_data = [addq(d[0]), addq(d[9]), d[1], addq(d[2]), addq(d[3]), addq(d[12]), addq(d[13]), addq(d[10]), d[11], d[4]]
+            outline_samples = [addq(d[15])]
+            if is_custom:
+                # get the number of PAUIS
+                npids = "0"
+                if d[16] != "":
+                    npids = str(d[16].count(',') + 1)
+                outline_data.append(npids)
+                outline_samples.append(addq(d[16]))
+            txt_data += "[" + ",".join(outline_data) + "]"
+            txt_samples += "[" + ",".join(outline_samples) + "]"
+            if i == len(cluster_data)-1:
+                txt_data += "]"
+                txt_samples += "]"
+            else:
+                txt_data += ","
+                txt_samples += ","
+           
+        #now write data to file, and gzip for quicker loading into browser
+        #basic cluster data (no samples) 
+        with gzip.open("cluster_data" + ext + ".json.gz", "wb") as f:
+            f.write(txt_data.encode())
+        #sample names for each cluster
+        with gzip.open("sample_data" + ext + ".json.gz", "wb") as f:
+            f.write(txt_samples.encode())
+
+
+        # write cluster id and specimen ids for Investigator
+        if is_custom:
+            with open("cluster_pids" + ext + ".json","w+") as outf:
+                print(json.dumps(sample_pids).replace(" ", ""), file = outf)
 
 if __name__ == "__main__":
     from master_backend import parse_setup
-    from utils import read_lexicon
     args = parse_setup()
-    lexicon = read_lexicon(args.lexicon)
-    lexicon.update({v:v for v in lexicon.values()})
     extension = args.region_extension
     if extension is None:
         extension = ['']
     else:
         extension.insert(0,'')
-    generate_display_tables(lexicon, args.host, extension)
+    generate_display_tables(extension)
