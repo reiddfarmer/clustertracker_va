48e0c7b54637864f56b2c47ace73521d77c9f77d
Jen Martin <jmart427@ucsc.edu>
2022-09-08 19:34:26 -0700

    merges updates from latest branch

--- build/data/process_metadata.py
+++ build/data/process_metadata.py
@@ -22,7 +22,7 @@
 #      -pids.tsv: list of sample names and corresponding sample IDs/PAUIs
 #
 # Example command line usage:
-#   python3 process_metadata.py -m samplemeta.tsv public.plusGisaid.latest.metadata.tsv -l state_and_county_lexicon.txt
+#   python3 process_metadata.py -m public.plusGisaid.latest.metadata.tsv -mx samplemeta.tsv -l state_and_county_lexicon.txt
 #-------------------------------------------------------------
 
 import re, csv
@@ -31,27 +31,29 @@ import sys, os #comment out for WDL
 sys.path.insert(1, os.path.join(os.path.abspath(__file__ + "/../../../"), "src/python/")) #comment out for WDL
 
 
-def process_metadata(lexiconfile, metadatafiles, extension=["_us"], isWDL = False):
+def process_metadata(lexiconfile, mfile, mfile_merge, extension=["_us"], isWDL = False):
     #== for WDL ===
     # isWDL = True
     #===
 
     if isWDL:
         lexiconfile = '~{state_and_county_lex}'
-        mfiles = ['~{samples}', '~{public_meta}']
-        ext = "_us"
+        mfile = '~{public_meta}'
+        mfile_merge = '~{samples}'
+        ext = "_us" 
         #names of airport sample input files
         airport_file_p = '~{airport_p}'
         airport_file_c = '~{airport_c}'
     else:
-        mfiles = metadatafiles
+        if type(mfile_merge) == list:
+            mfile_merge = mfile_merge[0]
         if extension is None:
             ext = "_us"
         else:
             ext = extension[0]
         #names of airport sample input files
-        airport_file_p = "F1a-qry-TravelerCOVIDNet-ToUCSC-Data-P-ALL-07052022.csv"
-        airport_file_c = "F1b-qry-TravelerCOVIDNet-ToUCSC-Data-C-ALL-07052022.csv"
+        airport_file_p = "F1a-qry-AirportCOVIDNet-ToUCSC-Data-P-ALL.csv"
+        airport_file_c = "F1b-qry-AirportCOVIDNet-ToUCSC-Data-C-ALL.csv"
 
     county_conversion = {}
     state_conversion = {}
@@ -68,7 +70,6 @@ def process_metadata(lexiconfile, metadatafiles, extension=["_us"], isWDL = Fals
                     state_conversion[alternative.upper()] = spent[0] 
 
     metadata = open("metadata_merged.tsv","w+") #output file for merged metadata, for US + CA county analysis
-    metadata_us = open("metadata_merged" + ext + ".tsv","w+") #output file for merged metadata, for US analysis
     badsamples = open("rejected_samples.txt","w+") # file for rejected sample names
     region_assoc = open("sample_regions.tsv","w+") # file to store associations between sample ID and region name, for US + CA county analysis
     region_assoc_us = open("sample_regions" + ext + ".tsv","w+") # file to store associations between sample ID and region name, for US analysis
@@ -79,8 +80,7 @@ def process_metadata(lexiconfile, metadatafiles, extension=["_us"], isWDL = Fals
     pid_assoc = open("pids.tsv","w+") # file to store associations between sample ID and specimen_id (formerly PAUI or link_id)
     date_pattern = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
     #write metadata header
-    print("strain\tname\tpangolin_lineage\tnextclade_clade\tgisaid_accession\tcounty\tdate\tpaui\tsequencing_lab\tspecimen_id\tspecimen_accession_number\tgenbank_accession\tcountry", file = metadata)
-    print("strain\tname\tpangolin_lineage\tnextclade_clade\tgisaid_accession\tcounty\tdate\tpaui\tsequencing_lab\tspecimen_id\tspecimen_accession_number\tgenbank_accession\tcountry", file = metadata_us)
+    print("strain\tname\tpango_lineage\tnextclade_clade\tgisaid_accession\tcounty\tdate\tpaui\tsequencing_lab\tspecimen_id\tspecimen_accession_number\tgenbank_accession\tNextstrain_clade\tpangolin_lineage\tNextstrain_clade_usher\tpango_lineage_usher\tcountry", file = metadata)
     duplicates = set() #stores sample names of potential duplicates
 
     #read airport sample data
@@ -94,121 +94,124 @@ def process_metadata(lexiconfile, metadatafiles, extension=["_us"], isWDL = Fals
         for row in data:
             airport_data.append([str(row["Submitter Specimen ID"]),str(row["PAUI"]),row["GISAID_epi_isl"],row["Airport"]])
     
-    for f in mfiles:
-        with open(f) as inf:
-            print("parsing input metadata file: " + f)
-            fields = inf.readline().strip().split("\t")
-            # check which format the metadata is in based on header field names
-            if fields[0] == "usherID":
-                # CA metadata header: usherID,name,pango_lineage,nextclade_clade,gisaid_accession,county,collection_date,paui,sequencing_lab,specimen_id,specimen_accession_number
-                for entry in inf:
-                    has_valid_county = False
-                    fields = entry.split("\t")
-                    for i in range(len(fields)):
-                        fields[i] = fields[i].strip()
-                    fields.append("")
-                    fields.append("USA")
-                    #add item to merged metadata file for CA state analysis
-                    print("\t".join(fields), file = metadata_us)
 
-                    #For airports, override county info with airport
-                    ids = [fields[0],fields[1],fields[7],fields[-2],fields[-1]]
+    with open(mfile_merge) as inf:
+        print("parsing input metadata file: " + mfile_merge)
+        # CA metadata header: usherID,name,pango_lineage,nextclade_clade,gisaid_accession,county,collection_date,paui,sequencing_lab,specimen_id,specimen_accession_number 
+        fields = inf.readline() #skip header
+        for entry in inf:
+            has_valid_county = False
+            fields = entry.split("\t")
+            for i in range(len(fields)):
+                fields[i] = fields[i].strip()
+            fields.append("") #genbank_accession
+            fields.append("") #Nextstrain_clade (public metadata)
+            fields.append("") #pangolin_lineage (public metadata)
+            fields.append("") #Nextstrain_clade_usher (public metadata)
+            fields.append("") #pango_lineage_usher (public metadata)
+            fields.append("USA") #country
+            #add item to merged metadata file
+            print("\t".join(fields), file = metadata)
+
+            #For airports, override county info with airport
+            ids = [fields[0],fields[1],fields[7],fields[-2],fields[-1]]
+            isAirport = False
+            for item in airport_data:
+                if any(item[0] in id for id in ids):
+                    isAirport = True
+                    break
+                elif item[1] != "" and any(item[1] in id for id in ids):
+                    isAirport = True
+                    break
+                elif item[2] != "" and item[2] == fields[4]:
+                    isAirport = True
+                    break
+            #assign region for airport data
+            if isAirport:
+                if item[3] in airp_conversion:
+                    text = airp_conversion[item[3]].replace(" ", "_")
+                    print(fields[0] + "\t" + text, file = region_assoc)
+                    print(fields[0] + "\t" + text, file = region_assoc_us)
+                else:
                     isAirport = False
-                    for item in airport_data:
-                        if any(item[0] in id for id in ids):
-                            isAirport = True
-                            break
-                        elif item[1] != "" and any(item[1] in id for id in ids):
-                            isAirport = True
-                            break
-                        elif item[2] != "" and item[2] == fields[4]:
-                            isAirport = True
-                            break
-                    #assign region for airport data and add to merged metadata file
-                    if isAirport:
-                        if item[3] in airp_conversion:
-                            text = airp_conversion[item[3]].replace(" ", "_")
-                            print(fields[0] + "\t" + text, file = region_assoc)
-                            print(fields[0] + "\t" + text, file = region_assoc_us)
-                            print("\t".join(fields), file = metadata)
-                        else:
-                            isAirport = False
-
-                    #assign region for non-airport data
-                    if not isAirport:
-                        #assign to CA state in region file
-                        print(fields[0] + "\tCalifornia", file = region_assoc_us)
-                        #check if county is in lexicon and if so add to 
-                        # merged metadata file and sample regions file
-                        county = fields[5]
-                        if county != "":
-                            if county.upper() in (c.upper() for c in county_conversion): # convert all names to uppercase to avoid differences in case
-                                has_valid_county = True
-                                text = county_conversion[county.upper()].replace(" ", "_")
-                                print(fields[0] + "\t" + text, file = region_assoc)
-                                #add item to merged metadata file for CA county analysis
-                                print("\t".join(fields), file = metadata)
-                    #add sample names to list to check for duplicates; add date to date file
-                    if fields[0].startswith("USA/"):
-                        #add sample name if needed to check for duplicates later on
-                        duplicates.add(fields[0])
-                        #check for valid date in file name, use date field if invalid
-                        if not re.search(date_pattern, fields[0][-10:]):
-                            if re.search(date_pattern, fields[6]):
-                                print(fields[0] + "\t" + fields[6], file = date_file_us)
-                                if has_valid_county:
-                                    print(fields[0] + "\t" + fields[6], file = date_file)
-                    else:
-                        #check for valid date and add sample ID and date to sample dates file
-                        if re.search(date_pattern, fields[6]):
-                            print(fields[0] + "\t" + fields[6], file = date_file_us)
-                            if has_valid_county:
-                                print(fields[0] + "\t" + fields[6], file = date_file)
-                    #add Specimen ID to association file
-                    if fields[9] != "":
-                        print(fields[0] + "\t" + fields[9], file = pid_assoc)
+            #assign region for non-airport data
+            if not isAirport:
+                #assign to CA state in region file
+                print(fields[0] + "\tCalifornia", file = region_assoc_us)
+                #check if county is in lexicon and if so add to region association file
+                county = fields[5]
+                if county != "":
+                    if county.upper() in (c.upper() for c in county_conversion): # convert all names to uppercase to avoid differences in case
+                        has_valid_county = True
+                        text = county_conversion[county.upper()].replace(" ", "_")
+                        print(fields[0] + "\t" + text, file = region_assoc)
+            #add sample names to list to check for duplicates; add date to date file
+            if fields[0].startswith("USA/"):
+                #add sample name if needed to check for duplicates later on
+                duplicates.add(fields[0])
+                #check for valid date in file name, use date field if invalid
+                if not re.search(date_pattern, fields[0][-10:]):
+                    if re.search(date_pattern, fields[6]):
+                        print(fields[0] + "\t" + fields[6], file = date_file_us)
+                        if has_valid_county:
+                            print(fields[0] + "\t" + fields[6], file = date_file)
             else:
-                # public metadata header: strain,genbank_accession,date,country,host,completeness,length,Nextstrain_clade,pangolin_lineage,Nextstrain_clade_usher,pango_lineage_usher
-                for entry in inf:
-                    fields = entry.strip().split("\t")
-                    country = fields[0].split("/")[0]
-                    # check for US samples
-                    if country == "USA":
-                        state = fields[0].split("/")[1].split("-")[0]
-                        if state in state_conversion:
-                            #first check for CA state duplicates
-                            if (state == "CA") and (fields[0] in duplicates):
-                                print(fields[0], file = badsamples)
-                            else:
-                                #add item to merged metadata file
-                                newfields = []
-                                newfields.append(fields[0]) #sample name
-                                newfields.append("") #CDPH name
-                                newfields.append(fields[8]) #pangolin_lineage
-                                newfields.append(fields[7]) #nextclade_clade = Nextstrain_clade
-                                newfields.append("") #gisaid_accession
-                                newfields.append("") #county name
-                                newfields.append(fields[2]) #date
-                                newfields.append("") #Link ID (PAUI)
-                                newfields.append("") #sequencing_lab
-                                newfields.append("") #specimen_id
-                                newfields.append("") #specimen_accession_number
-                                newfields.append(fields[1]) #genbank_accession
-                                newfields.append("USA") #country
-                                print("\t".join(newfields), file = metadata_us)
-                                #add sample ID and state name to sample regions files
-                                text = state_conversion[state.upper()].replace(" ", "_")
-                                print(fields[0] + "\t" + text, file = region_assoc_us)
-                                #filter out CA samples for CA county analysis
-                                if state != "CA":
-                                    print("\t".join(newfields), file = metadata)
-                                    print(fields[0] + "\t" + text, file = region_assoc)
+                #check for valid date and add sample ID and date to sample dates file
+                if re.search(date_pattern, fields[6]):
+                    print(fields[0] + "\t" + fields[6], file = date_file_us)
+                    if has_valid_county:
+                        print(fields[0] + "\t" + fields[6], file = date_file)
+            #add Specimen ID to association file
+            if fields[9] != "":
+                print(fields[0] + "\t" + fields[9], file = pid_assoc)
+
+    with open(mfile) as inf:
+        print("parsing input metadata file: " + mfile)
+        # public metadata header: strain,genbank_accession,date,country,host,completeness,length,Nextstrain_clade,pangolin_lineage,Nextstrain_clade_usher,pango_lineage_usher
+        fields = inf.readline() #skip header
+        for entry in inf:
+            fields = entry.strip().split("\t")
+            country = fields[0].split("/")[0]
+            #first, remove duplicate CDPH data
+            if country == "USA" and fields[0] in duplicates:
+                print(fields[0], file = badsamples)
+            else:
+                #add item to merged metadata file
+                newfields = []
+                newfields.append(fields[0]) #sample name
+                newfields.append("") #CDPH name
+                newfields.append("") #pango_lineage (CDPH metadata)
+                newfields.append("") #nextclade_clade (CDPH metadata)
+                newfields.append("") #gisaid_accession
+                newfields.append("") #county name
+                newfields.append(fields[2]) #date
+                newfields.append("") #paui
+                newfields.append("") #sequencing_lab
+                newfields.append("") #specimen_id
+                newfields.append("") #specimen_accession_number
+                newfields.append(fields[1]) #genbank_accession
+                newfields.append(fields[7]) #Nextstrain_clade (public metadata)
+                newfields.append(fields[8]) #pangolin_lineage (public metadata)
+                newfields.append(fields[9]) #Nextstrain_clade_usher (public metadata)
+                newfields.append(fields[10]) #pango_lineage_usher (public metadata)
+                newfields.append(fields[3]) #country
+                print("\t".join(newfields), file = metadata)
+                # get region for US samples
+                if country == "USA":
+                    state = fields[0].split("/")[1].split("-")[0]
+                    if state in state_conversion:
+                        #add sample ID and state name to sample regions files
+                        text = state_conversion[state.upper()].replace(" ", "_")
+                        print(fields[0] + "\t" + text, file = region_assoc_us)
+                        #filter out CA samples for CA county analysis
+                        if state != "CA":
+                            print(fields[0] + "\t" + text, file = region_assoc)
+
     metadata.close()
     badsamples.close()
     region_assoc.close()
     date_file.close()
     pid_assoc.close()
-    metadata_us.close()
     region_assoc_us.close()
     date_file_us.close()
 
@@ -219,4 +222,4 @@ if __name__ == "__main__":
         extension = ["_us"]
     else:
         extension = args.region_extension
-    process_metadata(args.lexicon, args.metadata, extension)
+    process_metadata(args.lexicon, args.metadata, args.merge_metafile, extension)
