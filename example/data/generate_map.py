8d7f3a4331d16a707c18705c9b2f41bd6c930b48
Andrew Warren <aswarren@gmail.com>
2024-02-28 16:41:45 -0500

    updates on generate map to calculate z-score

--- example/data/generate_map.py
+++ example/data/generate_map.py
@@ -29,35 +29,58 @@ from matplotlib import colors
 
 #main function reads in the hardcoded_clusters.tsv file, filters the table down to the lexicon selection in the first column using the region column in the hardcoded_clusters.tsv file
 
-def main(input, lexicon, geojson, output):
+def main(hardcoded, clusterswapped, lexicon, geojson, output):
 
     #read in the hardcoded_clusters.tsv file
-    df = pd.read_csv(args.input, sep='\t', header=0)
+    df = pd.read_csv(hardcoded, sep='\t', header=0)
+    df2 = pd.read_csv(clusterswapped, sep='\t', header=0)
     #read in the lexicon file
     lexicon = pd.read_csv(args.lexicon, sep=',', header=None)
     #filter the table down to the lexicon selection in the first column using the region column in the hardcoded_clusters.tsv file
     df_selected = df[df['region'].isin(lexicon[0])]
+    
     #filter df_selected to those rows that do not have ":VA" as substring in the inferred_origin column
     #this hack for VA will need to be generalized for other regions
     df_selected = df_selected[~df_selected['inferred_origin'].str.contains(':VA')]
+    
     #using the counties in the region column in df_selected and the counties in the name(s) attribute in a geojson generate a chlopleth map of the number of rows in df_selected per county
     #read in the geojson file
         #read in the geojson file
+    
+    #get the total number of samples which have a :VA in the region column from df2
+    total_va_samples = df2[df2['region'].str.contains(':VA')].shape[0]
+    #get the total number of samples which have a :VA in the region column from df2 per region
+    total_va_samples_per_region = df2[df2['region'].str.contains(':VA')].groupby('region').size()
+
     gdf = gpd.read_file(args.geojson)
     #create a dictionary of the number of introductions per county
     introductions = defaultdict(int)
     for index, row in df_selected.iterrows():
         introductions[row['region']] += 1
+    
+    #get the total number of introductions
+    total_introductions = sum(introductions.values())
+    #using the ratio of introductions to samples calculate the z-score for each county
+    z_scores = {}
+    for county, introductions in introductions.items():
+        #get the total number of samples from the region
+        total_samples = total_va_samples_per_region[county]
+        #calculate the z-score
+        z_scores[county] = (introductions - total_samples * total_introductions / total_va_samples) / np.sqrt(total_samples * total_introductions / total_va_samples)
+    #add the z-scores to the gdf
+    gdf['z_scores'] = gdf['name'].map(z_scores)
     #add the introductions to the gdf
     gdf['introductions'] = gdf['name'].map(introductions)
     #create the chloropleth map
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
     #center the map and zoom on VA
     gdf = gdf.cx[-83.6753:-75.1664, 36.5408:39.4660]
-    gdf.plot(column='introductions', cmap='OrRd', linewidth=0.8, ax=ax, edgecolor='0.8')
+    #gdf.plot(column='introductions', cmap='OrRd', linewidth=0.8, ax=ax, edgecolor='0.8')
+    gdf.plot(column='z_scores', cmap='OrRd', linewidth=0.8, ax=ax, edgecolor='0.8')
     ax.axis('off')
     #add a colorbar
-    norm = colors.Normalize(vmin=gdf['introductions'].min(), vmax=gdf['introductions'].max())
+    #norm = colors.Normalize(vmin=gdf['introductions'].min(), vmax=gdf['introductions'].max())
+    norm = colors.Normalize(vmin=gdf['z_scores'].min(), vmax=gdf['z_scores'].max())
     cbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap='OrRd'), ax=ax, orientation='horizontal', fraction=0.05, pad=0.05)
     cbar.set_label('Number of introductions')
     #save the chloropleth map
@@ -68,10 +91,11 @@ def main(input, lexicon, geojson, output):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This program converts hardcoded_clusters.tsv to a generalized EpiHiper seeding and creates a chloropleth map of the introductions into the state of VA with mean and std. dev calculated per variant per county")
     parser.add_argument("-i", "--input", help="The input hardcoded_clusters.tsv file", required=True)
+    parser.add_argument("-j", "--input2", help="The input clusterswapped.tsv file", required=True)
     parser.add_argument("-l", "--lexicon", help="The input lexicon file", required=True)
     parser.add_argument("-d", "--save_dir", help="The output directory", required=True)
     parser.add_argument("-n", "--save_name", help="The output name", required=True)
     #option for the geojson to use for the chloropleth map
     parser.add_argument("-g", "--geojson", help="The input geojson file", required=True)
     args = parser.parse_args()
-    main(input, lexicon, pangolin, output)
+    main(args.input, args.input2, args.lexicon, args.pangolin, args.output)
